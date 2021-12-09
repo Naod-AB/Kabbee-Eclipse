076e764ad69bbc3a0996fa767d1dee1234e170dc
Esrom <Esromtesfabrhan@gmail.com>
2021-12-09 21:45:18 +0300

    file name changes and new lists added

--- VMS_flutter/Bisha/visitor_management/lib/New folder/main.dart
+++ VMS_flutter/Bisha/visitor_management/lib/New folder/main.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 import 'package:flutter/material.dart';
-import 'names.dart';
+import 'Student_names.dart';
 
 void main() {
   runApp(MaterialApp(
--- VMS_flutter/Bisha/visitor_management/lib/New folder/staff_names.dart
+++ VMS_flutter/Bisha/visitor_management/lib/New folder/staff_names.dart
@@ -2,15 +2,16 @@
 
 import 'buttons.dart';
 import 'package:flutter/material.dart';
-class NameList extends StatelessWidget {
+class StaffNameList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded( child:
-    Theme(data:Theme.of(context).copyWith(
-      scrollbarTheme:  ScrollbarThemeData(
-        thumbColor: MaterialStateProperty.all(Colors.blue),
-        crossAxisMargin: -10,
+    return Expanded(
+      child:Theme(
+        data:Theme.of(context).copyWith(
+          scrollbarTheme:  ScrollbarThemeData(
+            thumbColor: MaterialStateProperty.all(Colors.blue[100]),
+            crossAxisMargin: -10,
       ),
     ),
     child: Scrollbar( 
@@ -20,7 +21,7 @@ class NameList extends StatelessWidget {
      SingleChildScrollView (
        
        child: Column(children: const [
-          Buttons(btnName:"Abdu", btnColor: Colors.white,txtColor: Colors.blueAccent,width: 500,height: 50,),
+          Buttons(btnName:"Staff", btnColor: Colors.white,txtColor: Colors.blueAccent,width: 500,height: 50,),
           Buttons(btnName:"Abel", btnColor: Colors.white,txtColor: Colors.blueAccent,width: 500,height: 50,),
           Buttons(btnName:"Biniam", btnColor: Colors.white,txtColor: Colors.blueAccent,width: 500,height: 50,),
           Buttons(btnName:"Bana", btnColor: Colors.white,txtColor: Colors.blueAccent,width: 500,height: 50,),
@@ -34,6 +35,7 @@ class NameList extends StatelessWidget {
           Buttons(btnName:"Jolly", btnColor: Colors.white,txtColor: Colors.blueAccent,width: 500,height: 50,),
           Buttons(btnName:"Kevin", btnColor: Colors.white,txtColor: Colors.blueAccent,width: 500,height: 50,),
           Buttons(btnName:"Luam", btnColor: Colors.white,txtColor: Colors.blueAccent,width: 500,height: 50,),
+
       ],
     ),
     ),
@@ -46,4 +48,3 @@ class NameList extends StatelessWidget {
     
   }
 }
-//Reusable button
