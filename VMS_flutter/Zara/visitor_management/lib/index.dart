15bafcc8bc37a30a762410de57c9410e3aad0a96
Naod-A <naod904@yahoo.com>
2021-12-15 21:03:40 +0300

    minor changes Naod

--- VMS_flutter/Zara/visitor_management/lib/index.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:visitor_management/home.dart';
-
-void main() => runApp(const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-    ));
