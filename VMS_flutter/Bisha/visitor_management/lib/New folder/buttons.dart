6532f437a00b53ee7ff7a622754b2042b0bd006c
Esrom <Esromtesfabrhan@gmail.com>
2021-12-15 19:06:05 +0300

    clear unused symbols and signs

--- VMS_flutter/Bisha/visitor_management/lib/New folder/buttons.dart
+++ VMS_flutter/Bisha/visitor_management/lib/New folder/buttons.dart
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-// ignore_for_file: use_key_in_widget_constructors, non_constant_identifier_names
-=======
-// ignore_for_file: use_key_in_widget_constructors, must_be_immutable
->>>>>>> 198a50c019ce27c2f5b9887fe50938af9f8f0244
 
 import 'package:flutter/material.dart';
 
