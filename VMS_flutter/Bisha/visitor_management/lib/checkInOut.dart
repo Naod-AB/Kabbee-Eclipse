55850ccde5576ca517f9861a25b1e4e4d883ffbd
Esrom <Esromtesfabrhan@gmail.com>
2021-12-16 21:13:34 +0300

    time buttons changed to containers

--- VMS_flutter/Bisha/visitor_management/lib/checkInOut.dart
+++ VMS_flutter/Bisha/visitor_management/lib/checkInOut.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'buttons.dart';
 import 'home.dart';
 
-
 class Checkin1 extends StatelessWidget {
   const Checkin1({Key? key}) : super(key: key);
 
@@ -19,11 +18,26 @@ class Checkin1 extends StatelessWidget {
           'YOU ARE CHECKED IN AT',
           style: TextStyle(fontSize: 16, color: Colors.blue),
         ),
-        Buttons(
-          btnName: '7:30 AM',
-          height: 70.0,
+        //
+        Container(
+          height: 50,
+          width: 400,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.blue.shade300),
+          ),
+          child: Center(
+            child: Text(
+              "7:30 AM",
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.blue.shade500,
+              ),
+            ),
+          ),
         ),
         Padding(padding: EdgeInsets.all(30.0)),
+
         ActionButtons(
           btnName: 'DONE',
           width: 300.0,
@@ -40,34 +54,70 @@ class Checkout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-               Padding(padding: EdgeInsets.all(95)),
-              Text('YOU ARE CHECKED IN AT', style: TextStyle(fontSize: 16, color: Colors.blue),),
-              Buttons(
-                btnName: '8:00 AM',
-                height: 70.0,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(padding: EdgeInsets.all(95)),
+        Text(
+          'YOU ARE CHECKED IN AT',
+          style: TextStyle(fontSize: 16, color: Colors.blue),
+        ),
+
+        Container(
+          height: 50,
+          width: 400,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.blue.shade300),
+          ),
+          child: Center(
+            child: Text(
+              "8:00 AM",
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.blue.shade500,
               ),
-              Text(
-                'YOU ARE CHECKED OUT AT',
-                style: TextStyle(fontSize: 16, color: Colors.blue),
+            ),
+          ),
+        ),
+        Padding(padding: EdgeInsets.all(30.0)),
+        Text(
+          'YOU ARE CHECKED OUT AT',
+          style: TextStyle(fontSize: 16, color: Colors.blue),
+        ),
+
+        Container(
+          height: 50,
+          width: 400,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.blue.shade300),
+          ),
+          child: Center(
+            child: Text(
+              "8:00 PM",
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.blue.shade500,
               ),
-              Buttons(btnName: '8:00 PM',
-                height: 70.0,
-              ),
-             Padding(padding: EdgeInsets.all(10)),
-              Text(
-                'HAVE A GOOD EVENING \n SEE YOU TOMORROW',
-                style: TextStyle(fontSize: 16, color: Colors.blue),
-              ),
-               Padding(padding: EdgeInsets.all(30.0)),
-              ActionButtons( 
-                btnName: 'DONE',
-                width: 300.0,
-                page: StartingPage(),
-              )
-            ],
-          
+            ),
+          ),
+        ),
+
+        // Buttons(btnName: '8:00 PM',
+        //   height: 70.0,
+        // ),
+        Padding(padding: EdgeInsets.all(10)),
+        Text(
+          'HAVE A GOOD EVENING \n SEE YOU TOMORROW',
+          style: TextStyle(fontSize: 16, color: Colors.blue),
+        ),
+        Padding(padding: EdgeInsets.all(30.0)),
+        ActionButtons(
+          btnName: 'DONE',
+          width: 300.0,
+          page: StartingPage(),
+        )
+      ],
     );
   }
 }
