f2f1a8dab49a2b13917d9444db7293d387e2b497
Esrom <Esromtesfabrhan@gmail.com>
2021-12-17 21:39:14 +0300

    final clean code

--- VMS_flutter/Bisha/visitor_management/lib/selection.dart
+++ VMS_flutter/Bisha/visitor_management/lib/selection.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
 import 'buttons.dart';
