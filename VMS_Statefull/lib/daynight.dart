47ce76bf0fb23636bfc3aa0fde5594f5ec455901
Esrom <Esromtesfabrhan@gmail.com>
2022-01-26 23:07:58 +0300

    no more used variable removed

--- VMS_Statefull/lib/daynight.dart
+++ VMS_Statefull/lib/daynight.dart
@@ -4,7 +4,6 @@ String daynight = dayNight();
 
 String dayNight() {
   var now = DateTime.now();
-  var formattedTime = DateFormat('HH:mm a').format(now).toString();
   bool dayOrNight = now.hour > 6 && now.hour < 11 ? true : false;
   String bgI = dayOrNight ? 'day.jpg' : 'night.jpg';
   return bgI;
