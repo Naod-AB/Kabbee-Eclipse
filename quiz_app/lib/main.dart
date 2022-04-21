3d94323cd7f7d4b57b11949338d9d907dbaa654e
miki <milkyas.dev@gmail.com>
2022-04-21 18:01:53 +0300

    updated dependencies and made one main dart file

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/route_manager.dart';
-<<<<<<< HEAD
-=======
-import 'package:quiz_app/screens/category_screen/category_screen.dart';
->>>>>>> e1fc0efe46abe60b1467bcc944445362c66f46ab
 import 'controllers/profile_bindings.dart';
 import '../screens/profile_screen.dart';
 import 'theme.dart';
@@ -21,7 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: quizAppTheme().copyWith(
-         brightness: Brightness.light,
+        brightness: Brightness.light,
       ),
       initialBinding: ProfileBinding(),
       home: ProfileScreen(),
