9aced735b86dca2e48a821ac8646d3902d09c51c
miki <milkyas.dev@gmail.com>
2022-04-29 18:06:47 +0300

    modiefied question sample to save choices on submit

--- quiz_app/lib/questions_sample.dart
+++ quiz_app/lib/questions_sample.dart
@@ -6,16 +6,16 @@ void main() {
   runApp(QuestionSample2());
 }
 
-class RadioModel {
-  int id;
-  final String buttonText;
-  final String text;
+class ChosenModel {
+  final int questionNumber;
+  final String questionAnswer;
 
-  RadioModel(
-    this.id,
-    this.buttonText,
-    this.text,
-  );
+  ChosenModel(this.questionNumber, this.questionAnswer);
+
+  @override
+  String toString() {
+    return '{questionNumber: ${questionNumber}, questionAnswer: ${questionAnswer}}';
+  }
 }
 
 class QuestionControl extends GetxController {
@@ -47,6 +47,8 @@ class QuestionControl extends GetxController {
     },
   ].obs;
 
+  List<ChosenModel> chosenAnswers = [];
+
   RxList groupValue = [-1, 0, 5, 9, 13].obs;
   RxList value = [
     [0, 1, 2, 4],
@@ -63,7 +65,6 @@ class QuestionSample2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print('sample_data.length == ${controller.questions.length}');
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -92,14 +93,12 @@ class QuestionSample2 extends StatelessWidget {
                 // color: Colors.green,
                 child: PageView.builder(
                     itemCount: controller.questions.length,
-                    onPageChanged: (y) {
-                      print(
-                          'sample_data.length == ${controller.questions.length}');
-                      controller.qnIndex.value = y + 1;
+                    onPageChanged: (pageNumber) {
+                      controller.qnIndex.value = pageNumber + 1;
                     },
                     itemBuilder: (context, snapshot) {
                       var x = controller.questions[snapshot]['options'];
-                      // print('options list${x}');
+
                       return Column(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
@@ -137,7 +136,27 @@ class QuestionSample2 extends StatelessWidget {
                                           onChanged: (newValue) {
                                             controller.groupValue[snapshot] =
                                                 newValue as int;
-                                            print(x[index].toString());
+                                            // if (controller.chosenAnswers
+                                            //     .contains(controller
+                                            //             .questions[snapshot]
+                                            //         ['id'])) {
+                                            //   controller.chosenAnswers.removeAt(
+                                            //       controller.questions[snapshot]
+                                            //           ['id']);
+                                            //   // print(controller.chosenAnswers);
+                                            // }
+                                            // print(controller.chosenAnswers);
+
+                                            controller.chosenAnswers.add(
+                                                ChosenModel(
+                                                    controller
+                                                            .questions[snapshot]
+                                                        ['id'] as int,
+                                                    x[index].toString()));
+                                            print(controller.chosenAnswers);
+                                            // controller.chosenAnswers.add(1, 'test');
+
+                                            // print(controller.questions[0]);
                                           }),
                                     ),
                                   ),
@@ -149,7 +168,10 @@ class QuestionSample2 extends StatelessWidget {
                           // display done btn
                           controller.questions.length - 1 == snapshot
                               ? ElevatedButton(
-                                  onPressed: () {}, child: const Text('Done'))
+                                  onPressed: () {
+                                    print(controller.chosenAnswers[0]);
+                                  },
+                                  child: const Text('Done'))
                               : Container(),
                         ],
                       );
@@ -162,115 +184,3 @@ class QuestionSample2 extends StatelessWidget {
     );
   }
 }
-
-// class QuestionsSample extends StatefulWidget {
-//   @override
-//   _QuestionWidgetState createState() => _QuestionWidgetState();
-// }
-// class _QuestionWidgetState extends State<QuestionsSample> {
-//   List<RadioModel> questions = [];
-//   List<int> groupValue = [];
-//   List<List<int>> value = [];
-//   int qnIndex = 1;
-//   @override
-//   void initState() {
-//     super.initState();
-//     questions.add(RadioModel(false, 'A', 'April 18', 'text1'));
-//     questions.add(RadioModel(false, 'B', 'April 17', 'text2'));
-//     questions.add(RadioModel(false, 'C', 'April 16', 'text3'));
-//     questions.add(RadioModel(false, 'D', 'April 15', 'text4'));
-//     groupValue.add(0);
-//     groupValue.add(5);
-//     groupValue.add(9);
-//     groupValue.add(13);
-//     // value.add([0, 1]);
-//     // value.add([2, 3]);
-//     // value.add([4, 5]);
-//     // value.add([6, 7]);
-//     value.add([0, 1, 2, 4]);
-//     value.add([5, 6, 7, 8]);
-//     value.add([9, 10, 11, 12]);
-//     value.add([13, 14, 15, 16]);
-//   }
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text("ListItem"),
-//         ),
-//         body: Padding(
-//           padding: const EdgeInsets.all(10.0),
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               Text(
-//                 qnIndex.toString() + '/' + questions.length.toString(),
-//                 style: TextStyle(fontSize: 30),
-//               ),
-//               //page view
-//               SizedBox(
-//                 height: 500,
-//                 // color: Colors.green,
-//                 child: PageView.builder(
-//                     itemCount: questions.length,
-//                     onPageChanged: (y) {
-//                       setState(() {
-//                         qnIndex = y + 1;
-//                       });
-//                     },
-//                     itemBuilder: (context, snapshot) {
-//                       return Column(
-//                         mainAxisAlignment: MainAxisAlignment.center,
-//                         children: [
-//                           SizedBox(
-//                             height: 80.0 * questions.length,
-//                             // choice cards
-//                             child: ListView.builder(
-//                               itemCount: questions.length, //4
-//                               itemBuilder: (context, index) => ButtonBar(
-//                                 alignment: MainAxisAlignment.center,
-//                                 children: <Widget>[
-//                                   Container(
-//                                     width: 300,
-//                                     color: Colors.black12,
-//                                     child: RadioListTile(
-//                                       title: Row(
-//                                         children: [
-//                                           Text(questions[index].buttonText),
-//                                           Spacer(),
-//                                           Text(questions[index].text),
-//                                           Spacer(),
-//                                         ],
-//                                       ),
-//                                       controlAffinity:
-//                                           ListTileControlAffinity.trailing,
-//                                       groupValue: groupValue[
-//                                           snapshot], // group value[0] 0
-//                                       value: value[index]
-//                                           [snapshot], //value[0] 0,1
-//                                       onChanged: (newValue) => setState(() {
-//                                         groupValue[snapshot] = newValue as int;
-//                                       }),
-//                                     ),
-//                                   ),
-//                                 ],
-//                               ),
-//                             ),
-//                           ),
-//                           // display done btn
-//                           questions.length - 1 == snapshot
-//                               ? ElevatedButton(
-//                                   onPressed: () {}, child: const Text('Done'))
-//                               : Container(),
-//                         ],
-//                       );
-//                     }),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
