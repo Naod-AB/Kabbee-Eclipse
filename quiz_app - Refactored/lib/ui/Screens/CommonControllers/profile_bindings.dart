7defc5c7ecf2026451b1629d841532097abe30ef
Naod-A <nabraham@kabbee.org>
2022-06-02 00:52:21 +0300

    modified some refactored code

---
+++ quiz_app - Refactored/lib/ui/Screens/CommonControllers/profile_bindings.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+import 'profile_controllers.dart';
+
+class ProfileBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<ProfileController>(() => ProfileController(), fenix: true);
+  }
+}
