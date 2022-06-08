4a6597d340288081ad3975ae8d8bb0299581e955
asmerom <asmiejohn@gmail.com>
2022-06-08 10:29:01 +0300

    fix timer issue

--- quiz_app/lib/screens/Score/evalu_screen.dart
+++ quiz_app/lib/screens/Score/evalu_screen.dart
@@ -15,6 +15,7 @@ import '../../controllers/count_down.dart';
 import '../../widgets/common_components/appbar_evalu.dart';
 import '../../controllers/question_controller.dart';
 import '/widgets/pallete.dart';
+//import 'dart:async';
 
 class evaluationScreens extends StatelessWidget {
   evaluationScreens({Key? key, required this.icon, required this.path})
@@ -214,9 +215,8 @@ class evaluationScreens extends StatelessWidget {
                                     controller.count =
                                         await fetchCorrectAnswers();
                                     controller.isEnabled.value = false;
-
                                     double scorePercent = controller.count /
-                                        pController.questionApi!.length *
+                                        pcontroller.questionApi!.length *
                                         100;
                                     CourseScore score = CourseScore(
                                         courseName:
@@ -255,7 +255,7 @@ class evaluationScreens extends StatelessWidget {
                                 coursePercentage: scorePercent,
                                 userId: pController.userInfo.value!.id);
                             print("after clicking done button ");
-                            controller.isFinished = false;
+                            controller.isFinished = true;
 
                             if (score.coursePercentage != null) {
                               saveUserScore(score);
